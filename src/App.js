ba53c717bcc727f89157157eaa978a915c558f80
lbbiplob <biplab.jsnu@gmail.com>
2022-09-28 22:39:28 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+
 import './App.css';
+import logo from './images/logo.png'
+import { useEffect, useState } from 'react';
+
+
+
+
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='main-container'>
+        <header className='header-section'>
+          <img src={logo} alt="" />
+          <h2>Cricket Skill Activity </h2>
+        </header>
+        <h3>Cricate Skill-up Practice </h3>
+        <div>
+          
+        </div>
+
+      </div>
+      <div>
+        <h1>Summery section </h1>
+      </div>
     </div>
   );
 }
